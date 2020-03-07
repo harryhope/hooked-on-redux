4288e92e1130a9b3105f476b4eec6c857fd235d2
Harry Hope <harry.hope@gmail.com>
2020-03-07 17:45:10 -0500

    add index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+import { constant } from 'updeep'
+import get from 'lodash/get'
+import isArray from 'lodash/isArray'
+import {createReducer as createUpdeepReducer} from 'redux-updeep'
+import {useSelector, useDispatch} from 'react-redux'
+
+export const createReducer = (initialState = {}, namespace = 'REDACT', handlers = {}) =>
+  createUpdeepReducer(namespace, initialState, handlers)
+
+export const useRedactState = (selector, defaultState, namespace = 'REDACT') => {
+  const value = useSelector(state =>
+    _.get(state, selector, defaultState)
+  )
+
+  const dispatch = useDispatch()
+
+  const updateValue = newValue =>
+    dispatch({
+      payload: isArray(newValue) ? constant(newValue) : newValue,
+      path: selector,
+      type: `${namespace}/${}`
+    })
+
+  return [value, updateValue]
+}
