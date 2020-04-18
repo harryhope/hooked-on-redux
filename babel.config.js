6162e58e199409e6bb34a2139eb92b2720c700bf
Harry Hope <harry.hope@gmail.com>
2020-04-18 19:03:00 -0400

    use modules: false for example build

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
+  presets: [['@babel/preset-env', {modules: false}], '@babel/preset-react'],
   env: {
     test: {
       plugins: ['transform-es2015-modules-commonjs']
